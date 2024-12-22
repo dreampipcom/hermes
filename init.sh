dc210ef6de98dcde5d3e655ad197ca65871b304a
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-12-22 17:36:49 +0100

    ar(feat) [DPD-NAN]: [matrix]: exploring Matrix server

--- init.sh
+++ init.sh
@@ -57,6 +57,9 @@ docker network create \
 log "dp::hermes::(busy)::creating folder structure."
 cd ./matrix/server
 mkdir data
+gosu
+sudo chmod -R a+rw data
+sudo chown $(whoami):docker data
 cd $root_dir
 
 
@@ -66,8 +69,7 @@ origin="./matrix/server/_docker-compose.yml"
 destination="./matrix/server/docker-compose.yml"
 tmpfile=$(mktemp)
 cp -p $origin $tmpfile
-UID="$(whoami)"
-echo $UID
+UID=$(whoami)
 cat $origin | envsubst > $tmpfile && mv $tmpfile $destination
 
 log "dp::hermes::(busy)::preparing Hermes Matrix Home Server configuration files."
