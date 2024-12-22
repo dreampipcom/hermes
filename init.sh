0823aa647936bd5d5bbf9e58e88e7c6455f028c7
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-12-22 19:36:59 +0100

    ar(feat) [DPD-NAN]: [matrix]: exploring Matrix server

--- init.sh
+++ init.sh
@@ -57,12 +57,9 @@ docker network create \
 log "dp::hermes::(busy)::creating folder structure."
 cd ./matrix/server
 mkdir data
-mkdir data-syn
 gosu
 sudo chmod -R a+rw data
 sudo chown $(whoami):docker data
-sudo chmod -R a+rw data-syn
-sudo chown $(whoami):docker data-syn
 cd $root_dir
 
 
