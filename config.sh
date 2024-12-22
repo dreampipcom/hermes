944dd718744c8b4ee97acd9ee3958e91f9f68097
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-12-22 19:24:53 +0100

    ar(feat) [DPD-NAN]: [matrix]: exploring Matrix server

--- config.sh
+++ config.sh
@@ -29,7 +29,7 @@ log "dp::hermes::(busy)::preparing Hermes Matrix Synapsis configuration files."
 
 docker run -it --rm \
     --mount type=volume,src=synapse-data,dst=/data \
-    -e SYNAPSE_SERVER_NAME=dpip.cc \
+    -e SYNAPSE_SERVER_NAME="$HERMES_SERVER_NAME" \
     -e SYNAPSE_REPORT_STATS=yes \
     matrixdotorg/synapse:latest generate
 
