10a9f34398149d946bcc7b6dd1c6fa442f25403f
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-01-25 22:36:48 +0100

    ar(hermes): adding next cloud

---
+++ nextcloud/_config.php
@@ -0,0 +1,27 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '${HERMES_CLOUD_BASEPATH}',
+  'memcache.local' => '\OC\Memcache\APCu',
+  'memcache.distributed' => '\OC\Memcache\Redis',
+  'memcache.locking' => '\OC\Memcache\Redis',
+  'redis' => [
+     'host' => '${HERMES_CLOUD_DB_REDIS}',
+     'port' => 6379,
+
+  ],
+  'apps_paths' =>
+  array (
+    0 =>
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 =>
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+);
