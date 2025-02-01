4480fd5b7ea0028a8d4d9db78ae7d00ef7849860
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-02-01 10:24:54 +0100

    ar(infra): re-org dms

--- init0.sh
+++ init0.sh
@@ -81,5 +81,7 @@ docker stop hermes_net_chat_synapse hermes_net_chat_element hermes_net_chat_post
 docker rm hermes_net_chat_synapse hermes_net_chat_element hermes_net_chat_postgres # to sort
 cd $root_dir
 
+docker network rm hermes_net_chat hermes_net_cloud hermes_net_mail hermes_net_model_all hermes_net_model_auth hermes_net_model_chat hermes_net_model_cloud
+
 
 log "dp::hermes::${HERMES_ENV}::shutdown::(idle)::all good." 0
