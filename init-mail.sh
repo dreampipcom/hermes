35ebda5e7ee8cf78fdefb537d198746fec4f1ba1
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-02-03 09:39:29 +0100

    ar(infra): re-org dms.

--- init-mail.sh
+++ init-mail.sh
@@ -60,5 +60,12 @@ cd mail
 docker compose up -d
 cd $root_dir
 
+# ENABLE_OPENDKIM=1 (default):
+docker exec -it hermes-mail-server setup config dkim domain ${HERMES_MAIL_DOMAINS}
+
+# ENABLE_RSPAMD=1 + ENABLE_OPENDKIM=0:
+# docker exec -it hermes-mail-server setup config dkim domain example.com
+# docker exec -it hermes-mail-server setup config dkim domain another-example.com
+
 
 log "dp::hermes::mail::(idle)::all good." 0
