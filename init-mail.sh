591343fc758fe4b2f08e71f80aad674078563453
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-02-13 13:37:43 +0100

    ar(feat) dms

--- init-mail.sh
+++ init-mail.sh
@@ -63,7 +63,7 @@ setup_storage () {
 		chmod 600 ~/.passwd-s3fs
 		mkdir mail/data/email-data
 		touch mail/data/email-data/dummy
-		s3fs $HERMES_MAIL_S3_BUCKET mail/data/email-data -o nonempty
+		s3fs $HERMES_MAIL_S3_BUCKET mail/data/email-data -o nonempty -o passwd_file=~/.passwd-s3fs -o url=https://${HERMES_MAIL_S3_BUCKET}.${HERMES_MAIL_S3_HOST}
 		log "dp::hermes::mail::(busy):: Preparing Aemilia (Mail: DMS): Cloud storage created." 0
 }
 
